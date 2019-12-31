f1ba10f5dcd326f2e4daf8df8240367392b054ba
ourayuki <50820795+ourayuki@users.noreply.github.com>
2019-12-31 21:17:51 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,7 +1,7 @@
 /*
  * myled.c
  *
- * Copyright (C) 2019 TodaSayaka <s17c1701gv@s.chibakoudai.jp>
+ * Copyright (C) 2019 TodaSayaka <s17c1097lb@s.chibakoudai.jp>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 
 #include <linux/module.h>
 #include <linux/fs.h>
