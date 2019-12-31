e2e207c9ffd3067bc632f7379be8bd9297930be2
ourayuki <s17c1701gv@s.chibakoudai.jp>
2019-12-31 19:53:27 +0900

    initcomit

--- myled.c
+++ myled.c
@@ -1,12 +1,35 @@
+/*
+ * myled.c
+ *
+ * Copyright (C) 2019 TodaSayaka <s17c1701gv@s.chibakoudai.jp>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
+#include <linux/timer.h>
+#include <asm/delay.h>
+#include <linux/timer.h>
+#include <linux/delay.h>
 
-
-MODULE_AUTHOR("Matsumoto Koki");
+MODULE_AUTHOR("toda sayaka");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
@@ -15,78 +38,91 @@ static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
 static volatile u32 *gpio_base = NULL;
+static int led_list[2] = {21, 25};
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
-  char c;
-  if(copy_from_user(&c,buf,sizeof(char)))
-  return -EFAULT;
-  if(c == '0')gpio_base[10] = 1 << 25;
-  else if(c == '1')gpio_base[7] = 1 << 25;
+        char c;
+	int i = 0;
+        if(copy_from_user(&c,buf,sizeof(char)))
+                return -EFAULT;
 
-  printk(KERN_INFO "recive:%c\n",c);
-  return 1;
+        if(c == '0'){
+		for(i=0; i<5; i++){
+		gpio_base[10] = 1 << 25;
+		msleep(1000);
+                gpio_base[10] = 1 << 21;
+		gpio_base[7] = 1 << 25;
+		msleep(1000);
+		gpio_base[7] = 1 << 21;
+		}
+        }
+        else if(c == '1'){
+		for(i=0; i<5; i++){
+		gpio_base[10] = 1 << 25;
+                gpio_base[10] = 1 << 21;
+		msleep(1000);
+		gpio_base[7] = 1 << 25;
+		gpio_base[7] = 1 << 21;
+		msleep(1000);
+		}
+        }
+        return 1;
 }
-
-static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
-{
-  int size = 0;
-  char sushi[] = {0xF0,0x9F,0x8D,0xA3,0x0A}; //寿司の絵文字のバイナリ
-  if(copy_to_user(buf+size,(const char *)sushi, sizeof(sushi))){
-    printk( KERN_INFO "sushi : copy_to_user failed\n" );
-    return -EFAULT;
-  }
-  size += sizeof(sushi);
-  return size;
-}
-
 
 static struct file_operations led_fops = {
-  .owner = THIS_MODULE,
-  .write = led_write,
-  .read = sushi_read
+        .owner = THIS_MODULE,
+        .write = led_write
 };
 
 static int __init init_mod(void)
 {
-  int retval;
+        int retval;
+        int i;
+        gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+        
+        for(i = 0; i < 2; i++){
+                const u32 led = led_list[i];
+                const u32 index = led / 10;
+                const u32 shift = (led % 10) * 3;
+                const u32 mask = ~(0x7 << shift);
+                gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+        }
+        
+        retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
+        if(retval < 0){
+                printk(KERN_ERR "alloc_chrdev_region faimyled.\n");
+                return retval;
+        }
 
-  const u32 led = 25;
-  const u32 index = led/10;
-  const u32 shift = (led%10)*3;
-  const u32 mask = ~(0x7 << shift);
-  gpio_base = ioremap_nocache(0x3f200000,0xA0);
-  gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+        printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 
-  retval = alloc_chrdev_region(&dev,0,1,"myled");
-  if (retval<0){
-    printk(KERN_ERR "alloc region failed.\n");
-    return retval;
-  }
-  printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
-  cdev_init(&cdv, &led_fops);
-  retval = cdev_add(&cdv, dev, 1);
-  if(retval < 0){
-    printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
-    return retval;
-  }
-  cls = class_create(THIS_MODULE,"myled");   
-  if(IS_ERR(cls)){
-    printk(KERN_ERR "class_create failed.");
-    return PTR_ERR(cls);
-  }
-  device_create(cls,NULL,dev,NULL,"myled%d",MINOR(dev));
-  return 0;
+        cdev_init(&cdv, &led_fops);
+        cdv.owner = THIS_MODULE;
+        retval = cdev_add(&cdv, dev, 1);
+        if(retval < 0){
+                printk(KERN_ERR "cdev_add failed. major:%d, minor:%d\n",MAJOR(dev),MINOR(dev));
+                return retval;
+        }
+
+        cls = class_create(THIS_MODULE,"myled");
+        if(IS_ERR(cls)){
+                printk(KERN_ERR "class_create failed.");
+                return PTR_ERR(cls);
+        }
+        device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+
+        return 0;
 }
 
-static void __exit cleanup_mod(void) 
+static void __exit cleanup_mod(void)
 {
-  cdev_del(&cdv);
-  device_destroy(cls,dev);
-  class_destroy(cls);
-  unregister_chrdev_region(dev, 1);
-  printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+        cdev_del(&cdv);
+        device_destroy(cls, dev);
+        class_destroy(cls);
+        unregister_chrdev_region(dev, 1);
+        printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
 
-module_init(init_mod);     
+module_init(init_mod);
 module_exit(cleanup_mod);
